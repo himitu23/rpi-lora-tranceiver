e5fc81ffade3e765736cb080168a641330002742
himitu23 <tokina23centcircumflex@gmail.com>
2019-03-05 02:45:38 +0000

    [update] separate into config.h and add some comments for useful lora

--- dragino_lora_app/main.c
+++ dragino_lora_app/main.c
@@ -3,148 +3,21 @@
  * Copyright (c) 2018 Dragino
  *
  * http://www.dragino.com
+ * 
+ * modified by himitu23(@_tokina23)
+ * How to use:
+ *  1. make clean
+ *  2. make
+ *  3. if these are no error (and warnings), then you can use tranceriver
+ *  4.1. type in terminal "./dragino_lora_app sender", then you can transmit HELLO via lora
+ *  4.2. type in terminal "./dragino_lora_app sender KONNICHIWA",  then you can transmit KONNICHIWA via lroa
+ *  4.3. type in terminal "./dragino_lora_app 0" (0 is meaningless), then you can receive message with lora
+ * 
+ * @warning you have to configure Frequency and SF values to communicate others.
  *
  *******************************************************************************/
 
-#include <string>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <sys/time.h>
-#include <signal.h>
-#include <stdlib.h>
-
-#include <sys/ioctl.h>
-
-#include <wiringPi.h>
-#include <wiringPiSPI.h>
-
-
-// #############################################
-// #############################################
-
-#define REG_FIFO                    0x00
-#define REG_OPMODE                  0x01
-#define REG_FIFO_ADDR_PTR           0x0D
-#define REG_FIFO_TX_BASE_AD         0x0E
-#define REG_FIFO_RX_BASE_AD         0x0F
-#define REG_RX_NB_BYTES             0x13
-#define REG_FIFO_RX_CURRENT_ADDR    0x10
-#define REG_IRQ_FLAGS               0x12
-#define REG_DIO_MAPPING_1           0x40
-#define REG_DIO_MAPPING_2           0x41
-#define REG_MODEM_CONFIG            0x1D
-#define REG_MODEM_CONFIG2           0x1E
-#define REG_MODEM_CONFIG3           0x26
-#define REG_SYMB_TIMEOUT_LSB  		0x1F
-#define REG_PKT_SNR_VALUE			0x19
-#define REG_PAYLOAD_LENGTH          0x22
-#define REG_IRQ_FLAGS_MASK          0x11
-#define REG_MAX_PAYLOAD_LENGTH 		0x23
-#define REG_HOP_PERIOD              0x24
-#define REG_SYNC_WORD				0x39
-#define REG_VERSION	  				0x42
-
-#define PAYLOAD_LENGTH              0x40
-
-// LOW NOISE AMPLIFIER
-#define REG_LNA                     0x0C
-#define LNA_MAX_GAIN                0x23
-#define LNA_OFF_GAIN                0x00
-#define LNA_LOW_GAIN		    	0x20
-
-#define RegDioMapping1                             0x40 // common
-#define RegDioMapping2                             0x41 // common
-
-#define RegPaConfig                                0x09 // common
-#define RegPaRamp                                  0x0A // common
-#define RegPaDac                                   0x5A // common
-
-#define SX72_MC2_FSK                0x00
-#define SX72_MC2_SF7                0x70
-#define SX72_MC2_SF8                0x80
-#define SX72_MC2_SF9                0x90
-#define SX72_MC2_SF10               0xA0
-#define SX72_MC2_SF11               0xB0
-#define SX72_MC2_SF12               0xC0
-
-#define SX72_MC1_LOW_DATA_RATE_OPTIMIZE  0x01 // mandated for SF11 and SF12
-
-// sx1276 RegModemConfig1
-#define SX1276_MC1_BW_125                0x70
-#define SX1276_MC1_BW_250                0x80
-#define SX1276_MC1_BW_500                0x90
-#define SX1276_MC1_CR_4_5            0x02
-#define SX1276_MC1_CR_4_6            0x04
-#define SX1276_MC1_CR_4_7            0x06
-#define SX1276_MC1_CR_4_8            0x08
-
-#define SX1276_MC1_IMPLICIT_HEADER_MODE_ON    0x01
-
-// sx1276 RegModemConfig2
-#define SX1276_MC2_RX_PAYLOAD_CRCON        0x04
-
-// sx1276 RegModemConfig3
-#define SX1276_MC3_LOW_DATA_RATE_OPTIMIZE  0x08
-#define SX1276_MC3_AGCAUTO                 0x04
-
-// preamble for lora networks (nibbles swapped)
-#define LORA_MAC_PREAMBLE                  0x34
-
-#define RXLORA_RXMODE_RSSI_REG_MODEM_CONFIG1 0x0A
-#ifdef LMIC_SX1276
-#define RXLORA_RXMODE_RSSI_REG_MODEM_CONFIG2 0x70
-#elif LMIC_SX1272
-#define RXLORA_RXMODE_RSSI_REG_MODEM_CONFIG2 0x74
-#endif
-
-// FRF
-#define        REG_FRF_MSB              0x06
-#define        REG_FRF_MID              0x07
-#define        REG_FRF_LSB              0x08
-
-#define        FRF_MSB                  0xD9 // 868.1 Mhz
-#define        FRF_MID                  0x06
-#define        FRF_LSB                  0x66
-
-// ----------------------------------------
-// Constants for radio registers
-#define OPMODE_LORA      0x80
-#define OPMODE_MASK      0x07
-#define OPMODE_SLEEP     0x00
-#define OPMODE_STANDBY   0x01
-#define OPMODE_FSTX      0x02
-#define OPMODE_TX        0x03
-#define OPMODE_FSRX      0x04
-#define OPMODE_RX        0x05
-#define OPMODE_RX_SINGLE 0x06
-#define OPMODE_CAD       0x07
-
-// ----------------------------------------
-// Bits masking the corresponding IRQs from the radio
-#define IRQ_LORA_RXTOUT_MASK 0x80
-#define IRQ_LORA_RXDONE_MASK 0x40
-#define IRQ_LORA_CRCERR_MASK 0x20
-#define IRQ_LORA_HEADER_MASK 0x10
-#define IRQ_LORA_TXDONE_MASK 0x08
-#define IRQ_LORA_CDDONE_MASK 0x04
-#define IRQ_LORA_FHSSCH_MASK 0x02
-#define IRQ_LORA_CDDETD_MASK 0x01
-
-// DIO function mappings                D0D1D2D3
-#define MAP_DIO0_LORA_RXDONE   0x00  // 00------
-#define MAP_DIO0_LORA_TXDONE   0x40  // 01------
-#define MAP_DIO1_LORA_RXTOUT   0x00  // --00----
-#define MAP_DIO1_LORA_NOP      0x30  // --11----
-#define MAP_DIO2_LORA_NOP      0xC0  // ----11--
-
-// #############################################
-// #############################################
-//
-typedef bool boolean;
-typedef unsigned char byte;
+#include "config.h"
 
 static const int CHANNEL = 0;
 
@@ -157,10 +30,8 @@ byte receivedbytes;
 enum sf_t { SF7=7, SF8, SF9, SF10, SF11, SF12 };
 
 /*******************************************************************************
- *
  * Configure these values!
- *
- *******************************************************************************/
+ * *******************************************************************************/
 
 // SX1272 - Raspberry connections
 int ssPin = 6;
@@ -168,12 +39,140 @@ int dio0  = 7;
 int RST   = 0;
 
 // Set spreading factor (SF7 - SF12)
-sf_t sf = SF7;
+sf_t sf = SF7;                                          /*********Config here**********/
 
-// Set center frequency
-uint32_t  freq = 868100000; // in Mhz! (868.1)
+// Set center frequency, forAS923
+//uint32_t  freq = 868100000; // in Mhz! (868.1)
+uint32_t  freq = 923200000; // in Mhz! (923.2)          /*********Config here**********/
 
-byte hello[32] = "HELLO";
+byte hello[32] = "HELLO";//message of transmitted
+
+//tranceiver main function
+int main (int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf ("Usage: argv[0] sender|rec [message]\n");
+        exit(1);
+    }
+
+    //configure PIN assign
+    wiringPiSetup () ;
+    pinMode(ssPin, OUTPUT);
+    pinMode(dio0, INPUT);
+    pinMode(RST, OUTPUT);
+
+    //setting with board
+    wiringPiSPISetup(CHANNEL, 500000);
+
+    SetupLoRa();
+
+    /************SENDER************/
+    if (!strcmp("sender", argv[1])) {
+        opmodeLora();
+        // enter standby mode (required for FIFO loading))
+        opmode(OPMODE_STANDBY);
+
+        writeReg(RegPaRamp, (readReg(RegPaRamp) & 0xF0) | 0x08); // set PA ramp-up time 50 uSec
+
+        configPower(23);
+
+        printf("Send packets at SF%i on %.6lf Mhz.\n", sf,(double)freq/1000000);
+        printf("------------------\n");
+
+        if (argc > 2)
+            strncpy((char *)hello, argv[2], sizeof(hello));
+
+        while(1) {
+            txlora(hello, strlen((char *)hello));
+            delay(5000);
+        }
+    
+    /***********RECEIVER***********/
+    } else {
+
+        // radio init
+        opmodeLora();
+        opmode(OPMODE_STANDBY);
+        opmode(OPMODE_RX);
+        printf("Listening at SF%i on %.6lf Mhz.\n", sf,(double)freq/1000000);
+        printf("------------------\n");
+        while(1) {
+            receivepacket(); 
+            delay(1);
+        }
+
+    }
+
+    return (0);
+}
+
+//receive a packet via lora here
+void receivepacket() {
+
+    long int SNR;
+    int rssicorr;
+
+    if(digitalRead(dio0) == 1)
+    {
+        if(receive(message)) {
+            byte value = readReg(REG_PKT_SNR_VALUE);
+            if( value & 0x80 ) // The SNR sign bit is 1
+            {
+                // Invert and divide by 4
+                value = ( ( ~value + 1 ) & 0xFF ) >> 2;
+                SNR = -value;
+            }
+            else
+            {
+                // Divide by 4
+                SNR = ( value & 0xFF ) >> 2;
+            }
+            
+            if (sx1272) {
+                rssicorr = 139;
+            } else {
+                rssicorr = 157;
+            }
+
+            printf("Packet RSSI: %d, ", readReg(0x1A)-rssicorr);
+            printf("RSSI: %d, ", readReg(0x1B)-rssicorr);
+            printf("SNR: %li, ", SNR);
+            printf("Length: %i", (int)receivedbytes);
+            printf("\n");
+            printf("Payload: %s\n", message);
+
+        } // received a message
+
+    } // dio0=1
+}
+
+//transmit a packet via lora here
+void txlora(byte *frame, byte datalen) {
+
+    // set the IRQ mapping DIO0=TxDone DIO1=NOP DIO2=NOP
+    writeReg(RegDioMapping1, MAP_DIO0_LORA_TXDONE|MAP_DIO1_LORA_NOP|MAP_DIO2_LORA_NOP);
+    // clear all radio IRQ flags
+    writeReg(REG_IRQ_FLAGS, 0xFF);
+    // mask all IRQs but TxDone
+    writeReg(REG_IRQ_FLAGS_MASK, ~IRQ_LORA_TXDONE_MASK);
+
+    // initialize the payload size and address pointers
+    writeReg(REG_FIFO_TX_BASE_AD, 0x00);
+    writeReg(REG_FIFO_ADDR_PTR, 0x00);
+    writeReg(REG_PAYLOAD_LENGTH, datalen);
+
+    // download buffer to the radio FIFO
+    writeBuf(REG_FIFO, frame, datalen);
+    // now we actually start the transmission
+    opmode(OPMODE_TX);
+
+    printf("send: %s\n", frame);
+}
+
+
+/*********************************************
+ *       Other Setting Functions Below       * 
+ * *******************************************/
 
 void die(const char *s)
 {
@@ -329,44 +328,7 @@ boolean receive(char *payload) {
     return true;
 }
 
-void receivepacket() {
 
-    long int SNR;
-    int rssicorr;
-
-    if(digitalRead(dio0) == 1)
-    {
-        if(receive(message)) {
-            byte value = readReg(REG_PKT_SNR_VALUE);
-            if( value & 0x80 ) // The SNR sign bit is 1
-            {
-                // Invert and divide by 4
-                value = ( ( ~value + 1 ) & 0xFF ) >> 2;
-                SNR = -value;
-            }
-            else
-            {
-                // Divide by 4
-                SNR = ( value & 0xFF ) >> 2;
-            }
-            
-            if (sx1272) {
-                rssicorr = 139;
-            } else {
-                rssicorr = 157;
-            }
-
-            printf("Packet RSSI: %d, ", readReg(0x1A)-rssicorr);
-            printf("RSSI: %d, ", readReg(0x1B)-rssicorr);
-            printf("SNR: %li, ", SNR);
-            printf("Length: %i", (int)receivedbytes);
-            printf("\n");
-            printf("Payload: %s\n", message);
-
-        } // received a message
-
-    } // dio0=1
-}
 
 static void configPower (int8_t pw) {
     if (sx1272 == false) {
@@ -403,77 +365,3 @@ static void writeBuf(byte addr, byte *value, byte len) {
     unselectreceiver();                                                                                 
 }
 
-void txlora(byte *frame, byte datalen) {
-
-    // set the IRQ mapping DIO0=TxDone DIO1=NOP DIO2=NOP
-    writeReg(RegDioMapping1, MAP_DIO0_LORA_TXDONE|MAP_DIO1_LORA_NOP|MAP_DIO2_LORA_NOP);
-    // clear all radio IRQ flags
-    writeReg(REG_IRQ_FLAGS, 0xFF);
-    // mask all IRQs but TxDone
-    writeReg(REG_IRQ_FLAGS_MASK, ~IRQ_LORA_TXDONE_MASK);
-
-    // initialize the payload size and address pointers
-    writeReg(REG_FIFO_TX_BASE_AD, 0x00);
-    writeReg(REG_FIFO_ADDR_PTR, 0x00);
-    writeReg(REG_PAYLOAD_LENGTH, datalen);
-
-    // download buffer to the radio FIFO
-    writeBuf(REG_FIFO, frame, datalen);
-    // now we actually start the transmission
-    opmode(OPMODE_TX);
-
-    printf("send: %s\n", frame);
-}
-
-int main (int argc, char *argv[]) {
-
-    if (argc < 2) {
-        printf ("Usage: argv[0] sender|rec [message]\n");
-        exit(1);
-    }
-
-    wiringPiSetup () ;
-    pinMode(ssPin, OUTPUT);
-    pinMode(dio0, INPUT);
-    pinMode(RST, OUTPUT);
-
-    wiringPiSPISetup(CHANNEL, 500000);
-
-    SetupLoRa();
-
-    if (!strcmp("sender", argv[1])) {
-        opmodeLora();
-        // enter standby mode (required for FIFO loading))
-        opmode(OPMODE_STANDBY);
-
-        writeReg(RegPaRamp, (readReg(RegPaRamp) & 0xF0) | 0x08); // set PA ramp-up time 50 uSec
-
-        configPower(23);
-
-        printf("Send packets at SF%i on %.6lf Mhz.\n", sf,(double)freq/1000000);
-        printf("------------------\n");
-
-        if (argc > 2)
-            strncpy((char *)hello, argv[2], sizeof(hello));
-
-        while(1) {
-            txlora(hello, strlen((char *)hello));
-            delay(5000);
-        }
-    } else {
-
-        // radio init
-        opmodeLora();
-        opmode(OPMODE_STANDBY);
-        opmode(OPMODE_RX);
-        printf("Listening at SF%i on %.6lf Mhz.\n", sf,(double)freq/1000000);
-        printf("------------------\n");
-        while(1) {
-            receivepacket(); 
-            delay(1);
-        }
-
-    }
-
-    return (0);
-}
